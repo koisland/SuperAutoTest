4cb3fe808c5f23dd7a289dbb7899948b06322178
koisland <koshima789@gmail.com>
2022-11-15 22:31:55 -0800

    Updated SQL queries to update on value changes to avoid incrementing row id every update.

--- src/db/sql/insert_food.sql
+++ src/db/sql/insert_food.sql
@@ -1,12 +1,9 @@
-INSERT OR REPLACE INTO foods (
-    name,
-    tier,
-    effect,
-    pack
-)
-VALUES (
-    ?1,
-    ?2,
-    ?3,
-    ?4
-);
+INSERT INTO foods (name, tier, effect, pack)
+VALUES (?1, ?2, ?3, ?4) 
+ON CONFLICT(name, pack) DO UPDATE SET 
+    tier = ?2,
+    effect = ?3
+WHERE 
+    tier != ?2 OR 
+    effect != ?3
+;
