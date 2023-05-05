01e0939ac1ded302c6018b184437e63dad370100
Keith <koshima789@gmail.com>
2023-05-05 06:56:11 -0700

    Bumper version to v0.4.7. Included subdirectory docs/examples.

--- docs/examples/src/permanent_coconut.rs
+++ docs/examples/src/permanent_coconut.rs
@@ -1,6 +1,5 @@
 use saptest::{FoodName, Pet, PetName, Statistics, Team, TeamShopping, TeamViewer};
 
-
 /// Demonstrating how to get permanent [`Coconut`](saptest::FoodName::Coconut) using the [`Leech`](saptest::PetName::Leech), [`Gorilla`](saptest::PetName::Gorilla) and [`Parrot`](saptest::PetName::Parrot).
 /// * Shop mechanics and effect order.
 /// * https://superautopets.fandom.com/f/p/4400000000000044254
@@ -9,14 +8,7 @@ pub fn permanent_coconut() -> Team {
     let pets = [
         Some(Pet::try_from(PetName::Seagull).unwrap()),
         Some(Pet::try_from(PetName::Gorilla).unwrap()),
-        Some(
-            Pet::new(
-                PetName::Parrot,
-                Some(Statistics::new(5, 3).unwrap()),
-                1,
-            )
-            .unwrap(),
-        ),
+        Some(Pet::new(PetName::Parrot, Some(Statistics::new(5, 3).unwrap()), 1).unwrap()),
         Some(Pet::try_from(PetName::Leech).unwrap()),
     ];
     let mut team = Team::new(&pets, 5).unwrap();
